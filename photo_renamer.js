392538f0a4adc9d16ef47e2a33b5912ea285ef26
Austin <joha0033@gmail.com>
2019-02-28 12:30:48 -0700

    linted

--- photo_renamer.js
+++ photo_renamer.js
@@ -1,26 +1,24 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const photosDir = path.join(__dirname, "photos");
+const photosDir = path.join(__dirname, 'photos');
 let photoId = 0;
 const walkSync = (currentDirPath, callback) => {
-	fs.readdirSync(currentDirPath).forEach((name) => {
-		var filePath = path.join(currentDirPath, name);
-		var stat = fs.statSync(filePath);
-		if (stat.isFile()) {
-			console.log("isFile... ?");
-            
-			callback(filePath, stat);
-		} else if (stat.isDirectory()) {
-			walkSync(filePath, callback);
-		}
-	});
+  fs.readdirSync(currentDirPath).forEach((name) => {
+    const filePath = path.join(currentDirPath, name);
+    const stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      callback(filePath, stat);
+    } else if (stat.isDirectory()) {
+      walkSync(filePath, callback);
+    }
+  });
 };
 
-walkSync(photosDir, (filePath, stat) => {
-	fs.rename(filePath, `${photosDir}/image_${photoId}.jpg`, (err) => {
-		if (err) throw err;
-		console.log("Rename complete!");
-	});
-	photoId++;
+walkSync(photosDir, (filePath) => {
+  fs.rename(filePath, `${photosDir}/image_${photoId}.jpg`, (err) => {
+    if (err) throw err;
+    console.log('Rename complete!'); // eslint-disable-line no-console
+  });
+  photoId += 1;
 });
